5e2f716ec9767ef201e294c5f22038e3686772d1
Franklin Kingma <franklin@fourdigits.nl>
2018-07-13 13:32:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,4 @@ setup(name='collective.miscbehaviors',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
-
       )
